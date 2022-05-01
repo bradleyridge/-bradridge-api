f200cb7d041e9826254bd53ea8df74d22063fff9
Bradley Ridge <thebradridge@gmail.com>
2022-04-30 22:09:46 -0400

    init db

--- build/knexfile.js
+++ build/knexfile.js
@@ -11,5 +11,16 @@ module.exports = {
     seeds: {
       directory: './lib/database/seeds/development'
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    searchPath: process.env.DATABASE_SCHEMA || 'public',
+    migrations: {
+      directory: './lib/database/migrations'
+    },
+    seeds: {
+      directory: './lib/database/seeds/production'
+    }
   }
 };

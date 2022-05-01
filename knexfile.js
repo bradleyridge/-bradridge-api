74af57a8742023138fcc3ddd052fbc85dcc92cd1
Bradley Ridge <thebradridge@gmail.com>
2022-04-30 22:02:42 -0400

    add production db configuration

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,12 @@ module.exports = {
     searchPath: process.env.DATABASE_SCHEMA || 'public',
     migrations: { directory: './lib/database/migrations' },
     seeds: { directory: './lib/database/seeds/development' },
+  },
+  production: {
+    client: 'postgresql',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    searchPath: process.env.DATABASE_SCHEMA || 'public',
+    migrations: { directory: './lib/database/migrations' },
+    seeds: { directory: './lib/database/seeds/production' },
   }
 };
